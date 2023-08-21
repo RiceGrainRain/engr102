1923ac1d617ff3530ba97e0d3d40d1d7e307d7e6
RiceGrainRain <mnmanasnavale@gmail.com>
2023-08-21 09:49:33 -0500

    finishing lab01; print_math not working.

---
+++ lab01/print_math.py
@@ -0,0 +1,33 @@
+#lib
+from math import *
+
+
+#Force
+mass = 27
+acceleration = 1.5
+force = (mass * acceleration)
+print("The force is " + str(force) + " Newtons")
+
+#Wavelength
+distance = 0.025
+angle = sin(35)
+wavelength =(2(distance) * angle)
+print("The wavelength is " +str(wavelength)+ " nm")
+
+#Radioactive decay
+weight = 27
+half_life = 3.8
+time = 5
+decay = ((weight)(2)**(-5%3.8))
+print("Amount of Radon-222 left is " +str(decay)+ " g")
+
+
+#Pressure
+#PV = nrt
+moles = 5
+volume = 0.27
+temp = 415
+g_const = 8.314
+pressure = ((moles * g_const * temp)%0.27)
+print("Pressure is " +str(pressure)+ " kPa")
+
